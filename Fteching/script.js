b56ccc1bc1bfbedddc86979488d104283744b876
abhay giri <119098448+ABHAYGIRI143@users.noreply.github.com>
2024-08-14 16:05:13 +0530

    Add files via upload

---
+++ Fteching/script.js
@@ -0,0 +1,8 @@
+async function fetchData(params) {
+    const response =await fetch("https://jsonplaceholder.typicode.com/posts");
+    console.log(response); 
+    const data =await response.json();
+    console.log(data);
+}
+
+fetchData();
