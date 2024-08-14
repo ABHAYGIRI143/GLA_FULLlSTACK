08ea230d1223b8fcbac66db4b01615e9ee19944d
abhay giri <119098448+ABHAYGIRI143@users.noreply.github.com>
2024-08-14 16:05:26 +0530

    Add files via upload

---
+++ ConsoleProject/script.js
@@ -0,0 +1,32 @@
+// console.log("%c my name is Abhay","color:red;background:blue");
+// console.log("%c my name is Abhay","color:pink;background:black;padding:100px;border:2px solid white");
+// console.log("%c my name is Abhay","color:pink;background:black;padding:100px;border:2px solid blue");
+// console.error("%c my name is Abhay","color:pink;background:black;padding:100px;border:2px solid blue");
+// console.warn("%c my name is Abhay","color:pink;background:black;padding:100px;border:2px solid blue");
+// console.info("%c my name is Abhay","color:pink;background:black;padding:100px;border:2px solid blue");
+
+
+let arr=[1,4,5,7,8];
+
+// Map
+// let newArr=arr.map((ele,index,arr)=>{
+//     console.log(`${ele} ${index}`)
+// })
+
+// For Each
+// arr.forEach(element => {
+//    console.log(element) 
+// });
+
+//Filter
+// let newArr=arr.filter(ele=>{
+//     return ele%2==0;
+// })
+// console.log(newArr)
+
+// console.log(arr.slice(2,4))
+// console.log(arr.splice(2,4))
+
+// console.log(arr.join('-'))
+
+// console.log(arr.concat([1,4,5,6]))
